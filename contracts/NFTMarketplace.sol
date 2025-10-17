c32b24d5c85e868c3eaa4cca4ad6fd056f45714b
Murillo Ramos <murillo.tech.dev@gmail.com>
2025-10-16 21:03:02 -0300

    Basic setup files

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// Internal imports for the openzeppelin dependecies:
+import "openzeppelin/contracts/utils/Counter.sol";
+import "openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin/contracts/token/ERC721/IERC721.sol";
