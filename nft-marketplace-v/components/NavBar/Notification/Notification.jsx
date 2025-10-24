6d5a223533f20e975a6b96c97f40f9ebaecde8ff
Murillo Ramos <murillo.tech.dev@gmail.com>
2025-10-24 20:38:44 -0300

    Finalizing Notification.jsx Component

--- nft-marketplace-v/components/NavBar/Notification/Notification.jsx
+++ nft-marketplace-v/components/NavBar/Notification/Notification.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import Image from 'next/image'
+
+// Internal Import
+import Style from './Notification.module.css'
+import images from '../../../img'
 
 const Notification = () => {
-    return <div>Notification</div>
+    return (
+        <div className={Style.notification}>
+            <p>Notification</p>
+            <div className={Style.notification_box}>
+                <div className={Style.notification_box_img}>
+                    <Image src={images.user1}
+                     alt = "profile image"
+                     width={50}
+                     height={50} 
+                     className={Style.notification_box}
+                    />
+                </div>
+                <div className={Style.notification_box_info}>
+                    <h4>Murillo Ramos</h4> // HardCoding
+                    <p>Measure action your user...</p>
+                    <small>3 minutes ago</small>
+                </div>
+                <span className={Style.notification_box_new}></span>
+            </div>
+        </div>
+    )
 }
 
 export default Notification
