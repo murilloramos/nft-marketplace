e2d5b723459982ad889f0a0d9eeb3f2b4498b557
Murillo Ramos <murillo.tech.dev@gmail.com>
2025-10-24 20:16:59 -0300

    Finalizing Discover.jsx Component

--- nft-marketplace-v/components/NavBar/Discover/Discover.jsx
+++ nft-marketplace-v/components/NavBar/Discover/Discover.jsx
@@ -42,7 +42,9 @@ const Discover = () => {
         <div>
             {discover.map((el, i) => (
                 <div key = {i + 1} className = {Style.discover}>
-                    <Link href = {{pathname: `${el.link}`}}/>
+                    <Link href = {{pathname: `${el.link}`}}>
+                        {el.name}
+                    </Link>
                 </div>
             ))}
         </div>
